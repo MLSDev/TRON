a60d4f173944b237bcb42add1bb18d2d2ceae9e9
Denys Telezhkin <strangervir@gmail.com>
2019-03-26 14:16:58 +0200

    Update to Swift 5, Xcode 10.2.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }, failure: { error in
                 print(error)
             })
-        debugPrint(token ?? "")
+        debugPrint(token)
         // Override point for customization after application launch.
         return true
     }
